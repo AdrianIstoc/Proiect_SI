e43f9bf0e3c9da36125b5657fb9e0d6d20a69353
AdrianIstoc <adrian.istoc@student.tuiasi.ro>
2025-04-03 14:58:23 +0300

    mongo connection done. cred. sper. mi foame

--- mongo.py
+++ mongo.py
@@ -1,17 +1,29 @@
 from pymongo import MongoClient
-from dotenv import load_dotenv
-import os
+from dotenv import dotenv_values
 
-load_dotenv()
+config = dotenv_values(".env")
 
-mongo_uri = os.getenv("MONGO_URI")
+class MongoDBConnection:
+    def __init__(self, db_uri=None, db_name='crypto_manager'):
+        self.client = MongoClient(config["MONGO_URI"])
+        self.db = self.client[config["DB_NAME"]]
 
-client = MongoClient(mongo_uri)
-
-db = client["crypto_manager"]
-
-files_collection = db["files"]
-keys_collection = db["keys"]
-performance_collection = db["performance"]
-
-print("Conexiune realizată cu succes la MongoDB Atlas!")
+    def get_collection(self, collection_name):
+        return self.db[collection_name]
+    
+    def list_collections(self):
+        return self.db.list_collection_names()
+    
+    def create_collection(self, collection_name):
+        if collection_name not in self.list_collections():
+            self.db.create_collection(collection_name)
+            print(f"Colectia '{collection_name}' a fost creata...")
+        else:
+            print(f"Colectia '{collection_name}' exista deja...")
+    
+    def close_connection(self):
+        self.client.close()
+    
+    def get_db(self):
+        return self.db
+    
